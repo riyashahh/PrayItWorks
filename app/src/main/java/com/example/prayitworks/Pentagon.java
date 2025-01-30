dd58237d1c18eef678d525d877d1c1638a17e2d7
koalaztang <brtang@ualberta.ca>
2025-01-30 16:10:43 -0700

    added pentagon branch

---
+++ app/src/main/java/com/example/prayitworks/Pentagon.java
@@ -0,0 +1,12 @@
+package com.example.prayitworks;
+
+public class Pentagon extends Shape{
+    public Pentagon(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+}
