2ef8be5ef1dd3be862ebbf0ecd5ba5e6d7b43b4a
Mayank Khandelwal <146058309+Executioner-Cs@users.noreply.github.com>
2025-01-29 20:23:57 -0700

    Update Shape.java
    corrected

--- app/src/main/java/com/example/prayitworks/Shape.java
+++ app/src/main/java/com/example/prayitworks/Shape.java
@@ -4,10 +4,4 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    public Shape(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public abstract double getArea(); // Abstract method for calculating area
 }
