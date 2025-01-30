794acf6ce232ac88e3a3d0deae40bba0062b934c
koalaztang <brtang@ualberta.ca>
2025-01-30 16:59:44 -0700

    added color string field orange

--- app/src/main/java/com/example/prayitworks/Shape.java
+++ app/src/main/java/com/example/prayitworks/Shape.java
@@ -4,5 +4,12 @@ public abstract class Shape {
     private int x;
     private int y;
 
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract double getArea(); // Abstract method for calculating area
+
     String color = "orange";
 }
