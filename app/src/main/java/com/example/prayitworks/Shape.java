947dc86d76313dbc4ad9a44fecc9e056356cf0fb
koalaztang <brtang@ualberta.ca>
2025-01-30 17:19:57 -0700

    update to new Shape class

--- app/src/main/java/com/example/prayitworks/Shape.java
+++ app/src/main/java/com/example/prayitworks/Shape.java
@@ -4,12 +4,5 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    public Shape(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public abstract double getArea(); // Abstract method for calculating area
-
     String color = "pink";
 }
